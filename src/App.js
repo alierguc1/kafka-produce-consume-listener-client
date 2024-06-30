c77eaf0ce9fe98a9079e922c8bdf01b181f2c42d
Ali Ergüç <ergucali1@gmail.com>
2024-07-01 01:42:46 +0300

    Add Material Ui package and order components. Edited List Item Component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect,useRef } from 'react';
+import connection  from './utils/signalr';
+import axios from 'axios';
+import { HOST,SEND_PRODUCE_PREFIX } from './constraints/connection-const';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import './styles/ListItem.css';
+import './styles/ListItemText.css';
 
-function App() {
+
+const Chat = () => {
+  const [jsonInput, setJsonInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+
+  useEffect(() => {
+      connection.on("ReceiveMessage", (sendJsonMessage) => {
+          setMessages(messages => [...messages, { sendJsonMessage }]);
+      });
+
+      connection.start().catch(err => console.error(err.toString()));
+
+      return () => {
+          connection.off("ReceiveMessage");
+      };
+  }, []);
+
+  const sendMessage = async () => {
+      try {
+          await axios.post(`${HOST}/${SEND_PRODUCE_PREFIX}`, {
+              kafkaJsonMessage: jsonInput
+          });
+      } catch (error) {
+          console.error('There was an error sending the message!', error);
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <input type="text" value={jsonInput} onChange={e => setJsonInput(e.target.value)} placeholder="User" />
+          <button onClick={sendMessage}>Send</button>
+          <ul>
+          <List
+            sx={{
+              width: '100%',
+              maxWidth: '90%',
+              bgcolor: 'background.paper',
+              border: '1px solid black',
+              position: 'relative',
+              overflow: 'auto',
+              maxHeight: 300,
+              '& ul': { padding: 0 },
+            }}
+            subheader={<ul />}
+          >
+              {messages.map((msg, index) => (
+                <div className="list-item">
+              <ListItem>
+              <div className="list-item-text">
+                <ListItemText secondary={msg.sendJsonMessage} />
+                </div>
+              </ListItem>     
+              </div>
+              ))}
+             </List>         
+          </ul>
+      </div>
   );
-}
+};
 
-export default App;
+export default Chat;
---
+++ src/constraints/connection-const.js
@@ -0,0 +1,4 @@
+export const HOST = "https://localhost:7212";
+export const SEND_PRODUCE_PREFIX = "api/produce/sendProduce";
+export const HUB_PREFIX = "producehub";
+export const RECEIVE_METHOD_PREFIX = "ReceiveMessage";
